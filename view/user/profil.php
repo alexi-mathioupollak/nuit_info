87a64d05ea9561bd6d7201bbd0fc305f2336acfa
Rivals Felix <felix.rivals@etu.umontpellier.fr>
2021-12-02 22:33:37 +0100

    t

--- view/user/profil.php
+++ view/user/profil.php
@@ -1,11 +1,10 @@
 <?php
-    //require_once './config/BDD.php';
+    require_once './config/BDD.php';
 
     if ((isset($_SESSION['id']))){ //si une session existe déja (= utilisateur connecté) on redirige vers la page d'accueil
-        
-        echo '<p>id : $_SESSION['id']</p>';
-        echo '<p>Pseudonyme : $_SESSION['pseudo']</p>';
-        echo '<p>Email : $_SESSION['email']</p>';
+      echo '<p>id : $_SESSION['id']</p>';
+      echo '<p>Pseudonyme : $_SESSION['pseudo']</p>';
+      echo '<p>Email : $_SESSION['email']</p>';
     }
 
 
