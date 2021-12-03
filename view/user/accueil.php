59e7f8843ff25607f38f7ca2fc5372644dff0dbd
Rivals Felix <felix.rivals@etu.umontpellier.fr>
2021-12-03 06:59:32 +0100

    meme

--- view/user/accueil.php
+++ view/user/accueil.php
@@ -34,7 +34,7 @@ Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à Grand-Fort-Philippe),
     </main>
 
     <footer>
-        <a href="~rivalsf/nuit_info/assets/img/meme_easteregg.jpg">meme</a>
+        <a href="./assets/img/meme_easteregg.jpg">meme</a>
     </footer>
 
 </div>
