97c2f460142b1dc2378b573e14a5f9686a9121dd
Rivals Felix <felix.rivals@etu.umontpellier.fr>
2021-12-03 04:34:34 +0100

    kefl

--- view/user/profil.php
+++ view/user/profil.php
@@ -1,6 +1,16 @@
+<head>
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+  <link rel="stylesheet" href="./assets/css/vanilla-zoom.min.css">
+</head>
+
 <br/><br/>
 <br/><br/>
 <br/><br/>
+
+
 <form method="post">
   <div class="mb-3"><label class="form-label" for="email"><strong style="margin-right: 10px;">Identifiant</strong><br></label><?= $_SESSION['id'] ?></div>
   <div class="mb-3"><label class="form-label" for="email"><strong style="margin-right: 10px;">Pseudo</strong><br></label><?= $_SESSION['pseudo'] ?></div>
